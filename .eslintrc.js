4cc2f942dabf85975ee801156d18dc0e7a6f6d54
Reuben Pasquini <reuben@frickjack.com>
2021-06-21 12:09:27 -0500

    chore(lint): disable some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,4 +12,21 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "max-classes-per-file": "off",
+    "no-underscore-dangle": "off",
+    "import/extensions": "off",
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-multiple-empty-lines": "off",
+    "no-plusplus": "off",
+    "class-methods-use-this": "off",
+    "no-restricted-syntax": "off",
+    "function-paren-newline": "off",
+    "no-prototype-builtins": "off",
+    "import/no-extraneous-dependencies": "off",
+    "@typescript-eslint/lines-between-class-members": "off",
+  },
 };
